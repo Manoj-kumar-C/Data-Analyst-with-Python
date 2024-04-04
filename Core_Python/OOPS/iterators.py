e288ca63edcf5fb2ad69d7cfbe787a7dabc24013
Manoj-kumar-C <developermanoj2003@gmail.com>
2024-04-04 21:11:55 +0530

    update_soon

---
+++ Core_Python/OOPS/iterators.py
@@ -0,0 +1,4 @@
+# iterators  next(), iter()
+
+
+
--- Core_Python/data_types.py
+++ Core_Python/data_types.py
@@ -1,1 +1,15 @@
 # number, bool, string, list, tuple, set, dict
+
+var1 = 10
+var2 = 20.1
+var3 = "hi"
+# list 
+var4 = [1, 2, 3]
+# tuple
+var5 = (1, 2, 3)
+# set
+var6 = {1, 2, 3}
+# dict
+var7 = {"a": 1, "b": 2, "c": 3}
+
+print(var1, var2, var3, var4, var5, var6, var7)
